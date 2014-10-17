92d9a6c5ba0ba62817c89f2bd964a2498835fbe3
Matthew Pace <pace4@purdue.edu>
2014-10-17 13:00:46 -0400

    Messed around with json data, updated README

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Purdue Food Bot
 ==============
 
-Tweeting at this bot with the name of a food court will make it tweet back a random menu item to try.
+Tweeting at this bot with the name of a food court will make it tweet back a random menu item to try. Running on a Raspberry Pi.
 
 Made with [Tweepy](http://www.tweepy.org/).
 
--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import tweepy
+import json
 import os
+
+class StdOutListener(tweepy.StreamListener):
+  def __init__(self, api):
+    self.api = api
+    super(tweepy.StreamListener, self).__init__()
+  
+  def on_data(self, data):
+    json_data = json.loads(data)
+    print '@%s' % json_data['user']['screen_name']
+    return True
+
+  def on_error(self, status):
+    print status
 
 auth = tweepy.OAuthHandler(os.environ["FOOD_BOT_CK"], os.environ["FOOD_BOT_CS"])
 auth.set_access_token(os.environ["FOOD_BOT_TOKEN"], os.environ["FOOD_BOT_TOKEN_S"])
 
 api = tweepy.API(auth)
 
-api.update_status('Updating using OAuth authentication via Tweepy!')
+listener = StdOutListener(api)
+stream = tweepy.Stream(auth, listener)
+stream.filter(track=['@PurdueFoodBot'])
 
